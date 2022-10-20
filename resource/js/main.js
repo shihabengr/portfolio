6a3d46323ac0ed6836ad512b5ead5da94ac17b0b
Shihab Sarkar <114810193+shihabengr@users.noreply.github.com>
2022-10-21 01:52:08 +0600

    Add files via upload

---
+++ resource/js/main.js
@@ -0,0 +1,10 @@
+//MixitUp
+var mixer = mixitup('.maxp');
+//Scrollify
+$(function() {
+    $.scrollify({
+    section : ".example-classname",
+    setHeights:false,
+    updateHash:false,
+    });
+});
